da4665096c5f8b6cbc1b3bc15618363b1dcb0f9c
mjflorezg <mateo.florez@upr.edu>
2026-09-23 18:24:29 -0400

    Updated on 09-23-2026 at 18:24

--- pushgit.bat
+++ pushgit.bat
@@ -1,8 +1,9 @@
 @echo off
-:: Obtiene la fecha y hora de forma limpia y universal
-for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /value') do set datetime=%%I
-set now=%datetime:~4,2%-%datetime:~6,2%-%datetime:~0,4%
-set hour=%datetime:~8,2%:%datetime:~10,2%
+:: Obtiene la fecha y hora usando PowerShell (compatible con Windows 11 actual)
+for /f "usebackq tokens=1,2" %%A in (`powershell -Command "Get-Date -Format 'MM-dd-yyyy HH:mm'"`) do (
+    set now=%%A
+    set hour=%%B
+)
 
 :: Ejecuta las instrucciones de Git en Windows
 git add -A
